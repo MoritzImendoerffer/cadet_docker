4bf40319d4d766360065476840355dc70f70cbea
mMoritzImendoerffer <moritz.imendoerffer@mailfence.com>
2025-07-07 10:25:56 +0200

    improved logging

--- app/main.py
+++ app/main.py
@@ -5,9 +5,28 @@ from pydantic import BaseModel
 
 from app.serialization import dumps, loads_b64
 from CADETProcess.simulator import Cadet
+import socket
+import logging
+import time
 
 app = FastAPI()
 
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(hostname)s - %(message)s'
+)
+
+# Add hostname to all log messages
+class HostnameFilter(logging.Filter):
+    hostname = socket.gethostname()
+    def filter(self, record):
+        record.hostname = HostnameFilter.hostname
+        return True
+
+logger = logging.getLogger()
+logger.addFilter(HostnameFilter())
 
 class SimulateRequest(BaseModel):
     process_serialized: str  # base64-encoded dill payload
@@ -20,7 +39,9 @@ def get_status():
 
 @app.post("/simulate")
 def simulate(req: SimulateRequest):
-
+    
+    logger.info("Started simulation request")
+    started = time.time()
     try:
         process = loads_b64(req.process_serialized)
     except Exception as exc:
@@ -30,6 +51,9 @@ def simulate(req: SimulateRequest):
         results = Cadet().simulate(process)
     except Exception as exc:
         raise HTTPException(status_code=400, detail=f"Deserialization failed: {exc}")
+    
+    stopped = time.time()
+    logger.info(f"Finished simulation request in : {started-stopped} seconds")
     
     pickled = dumps(results)
     return {
