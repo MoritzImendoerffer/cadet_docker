b97e74a898ad035ba035b6553d62270ae42283ab
MoritzImendoerffer <moritz.imend@gmail.com>
2025-07-09 17:46:04 +0200

    added health endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # app/main.py
 import base64
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from pydantic import BaseModel
 
 from app.serialization import dumps, loads_b64
@@ -9,9 +9,11 @@ import socket
 import logging
 import time
 import datetime
+import threading
 
 app = FastAPI()
-
+# Simple busy flag
+is_busy = threading.Lock()
 
 # Configure logging
 logging.basicConfig(
@@ -37,10 +39,22 @@ def get_status():
     return {"status": "ok"}
 
 
+@app.get("/health")
+async def health_check(response: Response):
+    """Health endpoint for HAProxy."""
+    if is_busy.locked():
+        response.status_code = 503
+        return {"healthy": False, "ready": False}
+    else:
+        return {"healthy": True, "ready": True}
+
 @app.post("/simulate")
 def simulate(req: SimulateRequest):
 
     logger.info(f"Started simulation request at {datetime.datetime.now()}")
+    if not is_busy.acquire(blocking=False):
+        return {"error": "Server busy"}, 503
+    
     started = time.time()
     try:
         process = loads_b64(req.process_serialized)
@@ -49,13 +63,14 @@ def simulate(req: SimulateRequest):
 
     try:
         results = Cadet().simulate(process)
+        stopped = time.time()
+        logger.info(f"Finished simulation request in : {started-stopped} seconds")
+        
+        pickled = dumps(results)
+        return {
+            "results_serialized": base64.b64encode(pickled).decode(),
+        }
     except Exception as exc:
-        raise HTTPException(status_code=400, detail=f"Deserialization failed: {exc}")
-    
-    stopped = time.time()
-    logger.info(f"Finished simulation request in : {started-stopped} seconds")
-    
-    pickled = dumps(results)
-    return {
-        "results_serialized": base64.b64encode(pickled).decode(),
-    }
+        raise HTTPException(status_code=400, detail=f"Simulation failed: {exc}")
+    finally:
+        is_busy.release()
